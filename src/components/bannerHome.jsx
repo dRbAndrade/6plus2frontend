81013baaf3c2ff006471940ac493d3aaf5629dc5
luiza dragonetti <malludra@gmail.com>
2021-12-03 20:07:12 -0300

    modificações no carrousel e no cardproduct

--- src/components/bannerHome.jsx
+++ src/components/bannerHome.jsx
@@ -15,10 +15,11 @@ const BannerHome = ()=>{
                                    <Col sm={{span: 4}} className="bannerContent">
                                           <h1 className="bannerTitle">Grande título <br/>com frase de impacto</h1>
                                           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                                          <ButtonBlack buttonContent="Ver produtos"></ButtonBlack>
+                                          <Container className="buttonBanner">
+                                                 <ButtonBlack>Ver coleção</ButtonBlack>
+                                          </Container>
                                    </Col>
                                    <Col sm={{span: 5}} className="bannerImg d-flex justify-content-end">
-                                          
                                           <img src="https://i.imgur.com/ft3BsID.png" alt="" />
                                    </Col>
                             </Row>                           
--- src/components/cardProduct.jsx
+++ src/components/cardProduct.jsx
@@ -18,10 +18,10 @@ const CardProduct = ({product})=>{
          <Card.Img variant="top" src={product.image}/>
          <Container className="cardProductGreen">
          <Row className="d-flex w-100 h-100 m-0">
-             <Col sm={8} className="d-flex flex-column justify-content-between pt-4 ps-4 g-0">
-                <Card.Title><h2>{`R$ ${product.price.toFixed(2)}`}</h2></Card.Title>
-                <Card.Subtitle><h3>{product.title}</h3></Card.Subtitle>
-                <Card.Text><h4>{product.category}</h4></Card.Text>
+             <Col sm={9} className="d-flex flex-column pt-4 ps-4 g-0">
+                <Card.Title className="mb-0"><h2>{`R$ ${product.price.toFixed(2)}`}</h2></Card.Title>
+                <Card.Subtitle className="mb-0"><h3>{product.title}</h3></Card.Subtitle>
+                <Card.Text className="mb-0"><h4>{product.category}</h4></Card.Text>
              </Col>   
              <Col className="linkToProduct d-flex align-items-center g-0">
                 <ButtonBlack className="buttonCard" handleSubmit={()=>navigate(`/products/${product.id}`)}buttonIcon={<BsChevronRight className="arrowRight" />}></ButtonBlack>
