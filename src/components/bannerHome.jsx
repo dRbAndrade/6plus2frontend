ee831a243ae51474934cb7ebcfe76c8fd182db56
luiza dragonetti <malludra@gmail.com>
2021-12-06 16:05:53 -0300

    alterações na home

--- src/components/bannerHome.jsx
+++ src/components/bannerHome.jsx
@@ -1,7 +1,6 @@
 import '../styles/bannerHome.scss'
 import ButtonBlack from "./buttonBlack"
 import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 
 const BannerHome = ()=>{
@@ -12,8 +11,7 @@ const BannerHome = ()=>{
                      <Col sm={6} className="bannerContent d-flex flex-column justify-content-center align-items-center">
                             <h1>O parceiro do seu melhor movimento</h1>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                            <div className="buttonBanner"><ButtonBlack>Ver produtos</ButtonBlack></div>
-                            
+                            <div className="buttonBanner"><ButtonBlack>Ver produtos</ButtonBlack></div>                            
                      </Col>
                      <Col sm={6} className="imgBox" style={{backgroundImage: `url(https://i.imgur.com/jTtnqmB.png)`}}>
                         
--- src/components/bannerPromo.jsx
+++ src/components/bannerPromo.jsx
@@ -1,8 +1,6 @@
 import '../styles/bannerPromo.scss'
 import ButtonBlack from "./buttonBlack"
 import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
 
 const BannerPromo = ({bannerImageUrl, bannerTitle, bannerSubtitle, children})=>{
        return(
