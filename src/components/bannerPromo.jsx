81b769017869159f758c208efb09077082ae064d
dRbAndrade <chucreandrade@gmail.com>
2021-12-08 18:41:42 -0300

    Measurements table

--- src/components/bannerPromo.jsx
+++ src/components/bannerPromo.jsx
@@ -7,7 +7,6 @@ const BannerPromo = ({bannerImageUrl, bannerTitle, bannerSubtitle, children})=>{
        const navigate = useNavigate();
        return(
        <>
-<<<<<<< HEAD
         <Container className="bannerPromo">
           <div className="bannerPromoImg" style={{backgroundImage: `url(${bannerImageUrl})`}}></div>
           <div className="bannerPromoContent">
@@ -16,16 +15,6 @@ const BannerPromo = ({bannerImageUrl, bannerTitle, bannerSubtitle, children})=>{
               <ButtonBlack buttonLink="/products" handleSubmit={()=>navigate("/products")}>Ver coleção</ButtonBlack>
           </div>
         </Container>
-=======
-              <Container className="bannerPromo">
-                     <div className="bannerPromoImg" style={{backgroundImage: `url(${bannerImageUrl})`}}></div>
-                     <div className="bannerPromoContent">
-                            <h1>{bannerTitle}</h1>
-                            <p>{bannerSubtitle}</p>
-                            <ButtonBlack buttonLink="/products" handleSubmit={()=>navigate("/products")}>Ver coleção</ButtonBlack>
-                     </div>
-              </Container>
->>>>>>> 28fe42894c45ff1193052e5aaf679c305ea501be
        </>
        )
 }
--- src/components/measurement-guide.jsx
+++ src/components/measurement-guide.jsx
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap";
 import "../styles/measurement-guide.scss";
 import {BsXLg} from 'react-icons/bs'
+import "../styles/modeloTabela.scss"
 
 
 const MeasurementGuide = ()=>{
@@ -13,6 +14,31 @@ const MeasurementGuide = ()=>{
   return(
     <Container className="measurement-modal">
       <button className="measurement-icon" onClick={closeModal}><BsXLg/></button>
+      <table>
+      <thead>
+        <tr>
+          <th colspan="6">Guia de Medidas</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>34</td>
+          <td class="grey">36</td>
+          <td>38</td>
+          <td class="grey">40</td>
+          <td>42</td>
+          <td class="grey">44</td>
+        </tr>
+        <tr>
+          <td>22.5 cm</td>
+          <td class="grey">23.5 cm</td>
+          <td>24.5 cm</td>
+          <td class="grey">26 cm</td>
+          <td>27 cm</td>
+          <td class="grey">28 cm</td>
+        </tr>
+      </tbody>
+    </table>
     </Container>
   )
 
