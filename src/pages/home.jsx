0c6145ccd122a574be0705c421c2aaa636c87563
dRbAndrade <chucreandrade@gmail.com>
2021-12-10 16:01:06 -0300

    Fixed carousel display issue

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,15 +9,17 @@ import {Helmet} from "react-helmet"
 import Row from "react-bootstrap/Row"
 import Footer from "../components/footer"
 import { useNavigate } from 'react-router';
+import { useContext } from "react"
+import { ProductContext } from "../contexts/ProductContext"
 
 const Home = () => { 
   const navigate = useNavigate();
   const width = window.innerWidth;
+  const {products} = useContext(ProductContext);
   let carrosselPages = 6;
 
   if(width>600)carrosselPages = 3;
   if(width>900)carrosselPages = 2;
-
   return(
     <>
       <Helmet>
@@ -32,8 +34,11 @@ const Home = () => {
           <ButtonBlack handleSubmit={()=>navigate("/products")}>Ver produtos</ButtonBlack>
           </BannerPromo>
             <Row className="p-3 g-0 d-flex justify-content-center">
+            {products.length>0&&
+            <>
               <h2 className="categoryTitle">Os mais vendidos</h2>
               <Carrossel pages={carrosselPages}></Carrossel>
+            </>}
             </Row>
         </main>
       </div>
